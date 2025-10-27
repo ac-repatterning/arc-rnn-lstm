8c697f70cb4b12b19432292025ef38d80b66e7e1
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-27 20:59:30 +0000

    Add src/elements/intermediary.py

---
+++ src/elements/intermediary.py
@@ -0,0 +1,26 @@
+
+import typing
+
+import pandas as pd
+import sklearn
+
+class Intermediary(typing.NamedTuple):
+    """
+    The data type class ⇾ Intermediary<br><br>
+
+    Attributes<br>
+    ----------<br>
+    <b>training</b> : pandas.DataFrame
+        The training data of a gauge<br>
+
+    <b>testing</b> : pandas.DataFrame
+        The testing data of a gauge<br>
+
+    <b>scaler</b> : sklearn.preprocessing.MinMaxScaler
+        The scaler object<br>
+
+    """
+
+    training: pd.DataFrame
+    testing: pd.DataFrame
+    scaler: sklearn.preprocessing.MinMaxScaler = None
