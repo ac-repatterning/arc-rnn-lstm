f07e6537da3fc1228d3a9a14d899086c35a84730
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 17:15:53 +0000

    Update src/modelling/valuations.py: In Progress

--- src/modelling/valuations.py
+++ src/modelling/valuations.py
@@ -19,11 +19,30 @@ class Valuations:
         self.__scaler = scaler
 
         # Arguments
-        self.__m_arguments: dict = arguments.get('modelling')
+        self.__arguments = arguments
 
-        self.__rename = { arg: f'e_{arg}' for arg in self.__m_arguments.get('targets')}
-        self.__disjoint = list(set(self.__m_arguments.get('fields')).difference(
-            set(self.__m_arguments.get('targets'))))
+        # Scaling Arguments
+        self.__features = self.__arguments.get('scaling').get('features')
+
+        # Modelling Arguments
+        self.__fields, self.__targets, self.__disjoint = self.__get_modelling_arguments()
+        self.__rename = { arg: f'e_{arg}' for arg in self.__targets}
+
+    def __get_modelling_arguments(self):
+        """
+
+        :return:
+        """
+
+        m_arguments: dict = self.__arguments.get('modelling')
+
+        fields: list = m_arguments.get('fields')
+        targets: list = m_arguments.get('targets')
+
+        # The variables present within the [input] fields, but not the targets
+        disjoint: list = list(set(fields).difference(set(targets)))
+
+        return fields, targets, disjoint
 
     def exc(self, x_matrix: np.ndarray, frame: pd.DataFrame):
         """
@@ -34,3 +53,10 @@ class Valuations:
         """
 
         predictions: np.ndarray = self.__model.predict(x=x_matrix)
+
+        # The inverse transform structure
+        structure = frame.copy()[self.__disjoint][-x_matrix.shape[0]:]
+        structure.loc[:, self.__targets] = predictions
+        structure = structure.copy()[self.__features]
+
+        i_structure = self.__scaler.inverse_transform(structure.values)
