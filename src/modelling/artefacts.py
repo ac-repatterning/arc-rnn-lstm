b73d8a24af663f170673a549a62003da7c99008b
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 20:09:03 +0000

    Update src/modelling/artefacts.py: In Progress

--- src/modelling/artefacts.py
+++ src/modelling/artefacts.py
@@ -1,12 +1,10 @@
 """Module artefacts.py"""
-import logging
 import os
+
 import pandas as pd
+import sklearn
 import tensorflow as tf
 
-import src.elements.intermediary as itr
-import src.elements.master as mr
-import src.elements.sequences as sq
 import src.functions.streams
 
 
@@ -15,45 +13,30 @@ class Artefacts:
     Artefacts
     """
 
-    def __init__(self, arguments: dict):
+    def __init__(self, model: tf.keras.src.models.Sequential, scaler: sklearn.preprocessing.MinMaxScaler, arguments: dict, path: str):
         """
 
         :param arguments: A set of arguments vis-à-vis calculation & storage objectives.
         """
 
+        self.__model = model
+        self.__scaler = scaler
         self.__arguments = arguments
+        self.__path = path
 
         # Instances
         self.__streams = src.functions.streams.Streams()
 
-    def __history(self, model: tf.keras.src.models.Sequential, path: str):
+    def __history(self):
         """
 
-        :param model:
-        :param path:
         :return:
         """
 
-        history = pd.DataFrame(data=model.history.history)
+        history = pd.DataFrame(data=self.__model.history.history)
 
-        return self.__streams.write(blob=history, path=os.path.join(path, 'history.csv'))
+        return self.__streams.write(blob=history, path=os.path.join(self.__path, 'history.csv'))
 
-    # noinspection PyUnresolvedReferences
-    def exc(self, model: tf.keras.src.models.Sequential, sequences: sq.Sequences, intermediary: itr.Intermediary, master: mr.Master) -> str:
-        """
+    def exc(self):
 
-        :param model:
-        :param sequences:
-        :param intermediary:
-        :param master:
-        :return:
-        """
-
-        self.__history(model=model, path=master.path)
-
-
-
-
-
-
-        return 'in progress'
+        self.__history()
