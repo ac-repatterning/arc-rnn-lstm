4c9e115aa1397f3d9dede2db65fa5aa964f67853
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-28 20:08:21 +0000

    Update src/modelling/estimates.py: In Progress

--- src/modelling/estimates.py
+++ src/modelling/estimates.py
@@ -1,4 +1,12 @@
 
+import tensorflow as tf
+
+import src.elements.intermediary as itr
+import src.elements.master as mr
+import src.elements.sequences as sq
+import src.modelling.artefacts
+
+
 class Estimates:
     """
     Estimates
@@ -11,3 +19,19 @@ class Estimates:
         """
 
         self.__arguments = arguments
+
+    def exc(self, model: tf.keras.src.models.Sequential, sequences: sq.Sequences, intermediary: itr.Intermediary, master: mr.Master) -> str:
+        """
+
+        :param model:
+        :param sequences:
+        :param intermediary:
+        :param master:
+        :return:
+        """
+
+        src.modelling.artefacts.Artefacts(model=model, scaler=intermediary.scaler, arguments=self.__arguments, path=master.path)
+
+
+
+        return 'in progress'
