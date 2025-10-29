e22e534ed692c4ae6f78ee1ee17e1ef02fb9bca7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-10-29 14:41:23 +0000

    Update src/modelling/estimates.py: In Progress

--- src/modelling/estimates.py
+++ src/modelling/estimates.py
@@ -1,10 +1,11 @@
-
+import pandas as pd
 import tensorflow as tf
 
 import src.elements.intermediary as itr
 import src.elements.master as mr
 import src.elements.sequences as sq
 import src.modelling.valuations
+import src.functions.streams
 
 
 class Estimates:
@@ -19,6 +20,19 @@ class Estimates:
         """
 
         self.__arguments = arguments
+
+        # Instances
+        self.__streams = src.functions.streams.Streams()
+
+    def __persist(self, blob: pd.DataFrame, path: str):
+        """
+
+        :param blob:
+        :param path:
+        :return:
+        """
+
+        self.__streams.write(blob=blob, path=path)
 
     def exc(self, model: tf.keras.src.models.Sequential, sequences: sq.Sequences, intermediary: itr.Intermediary, master: mr.Master) -> str:
         """
